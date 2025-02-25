263490717eedfa86675f3d517180a7b80d850829
Phakaphol Dherachaisuphakij <godzk25@gmail.com>
2025-02-25 09:28:39 +0700

    commit a playground

---
+++ src/myfirstjava/Cat.java
@@ -0,0 +1,15 @@
+package src.myfirstjava;
+
+public class Cat {
+
+    public static int walk() {
+        System.out.println("Walk");
+        return 1;
+    }
+    public static void fc(){
+        int a = 14;
+        String student = "Fc : adam";
+        System.out.println(student);
+    }
+
+}
--- src/myfirstjava/Users.java
+++ src/myfirstjava/Users.java
@@ -1,19 +1,26 @@
 package src.myfirstjava;
-// Comment
-/* multiple line
-comment*/
-
 public class Users {
+   static String a = "amigo";
+   int b = 1;
     public static void main(String[] args) {
-        System.out.println("Hello World");
         System.out.print("Three..");
         System.out.println("Two..");
         try{
-            int[] myNumber = {1,2,3};
-            System.out.println(myNumber[4]);
+            if (1 == 1){
+              System.out.println("If else Statements");
+              System.out.println("Hello World" + " " + a);
+            }
+            else{
+                System.out.println("No its error");
+            }
+            Cat.walk();
+            Cat.fc();//adam
         }
         catch (Exception e){
             System.out.println("Error นะคร้าบบบ: " + e);
         }
+    }
+    public static void passWord(){
+
     }
 }
