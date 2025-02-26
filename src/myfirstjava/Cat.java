54db099215b2a97f54c5a99e4d910824d7cde0c7
Phakaphol Dherachaisuphakij <godzk25@gmail.com>
2025-02-26 09:54:59 +0700

    commit a string  using in java

--- src/myfirstjava/Cat.java
+++ src/myfirstjava/Cat.java
@@ -3,10 +3,16 @@ package src.myfirstjava;
 public class Cat {
 
     public static int walk() {
-        System.out.println("Walk");
+        /* THis is a
+        Walk Method*/
+        String b = "ing";
+        String c = "eat";
+        System.out.println("Walk"+ b +" " + c );
+
         return 1;
     }
     public static void fc(){
+        // this is fc method
         int a = 14;
         String student = "Fc : adam";
         System.out.println(student);
